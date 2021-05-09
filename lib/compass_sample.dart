c2932b9cd854c1394093ae86002c663adac320de
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-05-09 14:58:35 +0900

    Add: compass

--- lib/compass_sample.dart
+++ lib/compass_sample.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/utils/compass.dart';
+import 'package:provider/provider.dart';
 
 class CompassSample extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Compass sample'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('data')
-          ],
-        ),
+    return ChangeNotifierProvider<Compass>(
+      create: (_) => Compass(),
+      child: Consumer<Compass>(
+        builder: (context, compass, child) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Compass sample'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(compass.angle.toString()),
+                  Transform.rotate(
+                    angle: compass.angle,
+                    child: Image.asset('assets/compass.png'),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
